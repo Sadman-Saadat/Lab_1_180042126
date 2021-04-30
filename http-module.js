df6ebf0c262682a0cbfaee0e181f4460191972aa
Sadman Saadat <55857736+Sadman-Saadat@users.noreply.github.com>
2021-04-30 14:35:12 +0600

    completed loadcontent module

--- http-module.js
+++ http-module.js
@@ -1,15 +1,42 @@
 const http = require('http');
+const load = require('./loadContent');
 
 const server = http.createServer((req, res)=>{
     if(req.url == "/"){
-        res.write("<h1>This is base url</h1>");
+        res.write(load.index);
         res.end();
     }
 
-    else if(req.url == "/home"){
-        res.write("<h1>This is Home Page.</h1>")
+    else if(req.url == "/about"){
+        res.write(load.about);
         res.end();
     }
+
+    else if(req.url == "/blog"){
+        res.write(load.blog);
+        res.end();
+    }
+
+    else if(req.url == "/contact"){
+        res.write(load.contact);
+        res.end();
+    }
+
+    else if(req.url == "/pricing"){
+        res.write(load.pricing);
+        res.end();
+    }
+
+    else if(req.url == "/services"){
+        res.write(load.services);
+        res.end();
+    }
+
+    else if(req.url == "/work"){
+        res.write(load.work);
+        res.end();
+    }
+
     else{
         res.write("<h1>This Page doesn't exist</h1><br><a href = '/'>Go to Base</a>");
         res.end();
