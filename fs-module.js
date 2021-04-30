2acbb0d8ee515e59351bea46aac8d7591d41fef9
Sadman Saadat <55857736+Sadman-Saadat@users.noreply.github.com>
2021-04-30 12:30:41 +0600

    fs module

---
+++ fs-module.js
@@ -0,0 +1,41 @@
+//Synchronous and Asynchronouos Function
+
+//readfile
+//writefile
+//Appendfile
+//Delete
+//Rename
+
+const fs = require("fs");
+
+fs.writeFileSync('./Contents/demoFile.txt', 'We are learning nodeJs.');
+fs.appendFileSync('./Contents/demoFile.txt', 'We are learning nodeJs.');
+
+// fs.rename('contents/demoFile.txt','contents/renamedFile.txt', (err)=>{
+//     if(err){
+//         console.log(err);
+//     } else{
+//         console.log("Rename Successful");
+//     }
+// });
+
+console.log("before");
+
+fs.readFile('contents/renamedFile.txt', "utf-8", (err, data)=>{
+    if(err){
+        console.log(err);
+    } else {
+        fs.appendFileSync(
+            "./contents/renamedFile.txt", "Is this a synchronous process?"
+        );
+        fs.readFile("./Contents/renamedFile.txt", "utf-8", (err, data)=>{
+            if(err){
+                console.log(err);
+            } else{
+                console.log(data);
+            }
+        });
+    }
+});
+
+console.log("after");
